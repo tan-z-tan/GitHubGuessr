6d30ec2bcadb167c90f41ae636e67c64cdc48655
Makoto Tanji <tanji.makoto@gmail.com>
2023-09-09 00:00:03 +0900

    Commit sample data

---
+++ data/repo_with_snippets.ts
@@ -0,0 +1,17 @@
+export const githubPopularRepos = [
+  {
+    org: "facebook",
+    name: "facebook/react",
+    lang: "JavaScript",
+    desc: "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
+    star_num: 0,
+    fork_num: 0,
+    avatarURL: "",
+    url: "",
+    snippets: [
+      "dummy data",
+      "test data",
+      "sample data",
+    ]
+  }
+];
